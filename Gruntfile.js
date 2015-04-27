19bc00e41c5c0c42b5bc4320280959f8ccdee89b
Patrick O'Neill <patrick.oneill46@gmail.com>
2015-04-27 21:36:26 +0100

    Adding grunt and less Adding copy Reordering dom - removing h1

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt){
+
+	require('load-grunt-tasks')(grunt);
+
+	grunt.initConfig({
+
+		watch: {
+			less: {
+				files: ['less/*.less'],
+				tasks: ['less:development']
+			}
+		},
+		less: {
+			development: {
+				files: {
+		          "styles.css": "less/index.less"
+		        },
+		        options: {
+		          sourceMap: true,
+		          sourceMapURL: 'styles.css.map'
+		        }
+			}
+		}
+	});
+
+	grunt.registerTask('default', [
+		'less:development',
+		'watch'
+	]);
+}
